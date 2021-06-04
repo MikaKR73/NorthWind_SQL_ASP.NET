61a7d575f9c098c360d36c5375b09610ebc6f289
mika <mikan73.tili@outlook.com>
2021-06-04 14:51:49 +0300

    Northwind asp.net ohjelma visual studiolla. Toimii mutta on buginen ja pikkasen kesken vielä.

---
+++ HomeController/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HomeController.Models;
+
+namespace HomeController.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            if (Session["UserName"] == null)
+            {
+                return RedirectToAction("login", "home");
+            }
+            else
+            {
+                NorthwindOriginalEntities1 db = new NorthwindOriginalEntities1();
+                List<Products> model = db.Products.ToList();
+                db.Dispose();
+
+                return View(model);
+            }
+        }
+
+        public ActionResult TuoteKuva()
+        {
+            NorthwindOriginalEntities1 db = new NorthwindOriginalEntities1();
+            List<Products> model = db.Products.ToList();
+            db.Dispose();
+
+            return View(model);
+        }
+    }
+}
---
+++ HomeController/ViewModels/Yritykset.cs
@@ -0,0 +1,30 @@
+﻿using HomeController.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HomeController.ViewModels
+{
+    public class Yritykset
+    {
+        public Yritykset()
+        {
+            this.Orders = new HashSet<Yritykset>();
+        }
+
+        public int ShipperID { get; set; }
+        public Nullable<int> RegionID { get; set; }
+
+        [Display(Name = "Yritys")]
+
+        public string CompanyName { get; set; }
+        public string Phone { get; set; }
+
+        public string RegionDescription { get; set; }
+
+        public virtual ICollection<Yritykset> Orders { get; set; }
+        public virtual Region Region { get; set; }
+    }
+}
